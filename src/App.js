429a910a3b8ef627c4dd60cee5754120b8476aae
Abhishek Agarwal <agrawal.abhishek120201@gmail.com>
2023-08-17 01:27:07 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import React from 'react';
 import Front from './components/Home/FrontPage.jsx';
+import Category from './components/Category/Category.jsx';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    <Front/>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={ <Front/>} />
+        <Route path="/category" element={ <Category/>} />
+      </Routes>
+    </div>
   );
 }
 
