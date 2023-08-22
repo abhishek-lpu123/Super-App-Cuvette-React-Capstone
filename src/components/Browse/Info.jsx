2dc13d3d1ae8c6ed5b1a5c4249ec0ad21e325e0e
Abhishek Agarwal <agrawal.abhishek120201@gmail.com>
2023-08-22 21:39:00 +0530

    added user profile card

--- src/components/Browse/Info.jsx
+++ src/components/Browse/Info.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import ProfileImg from '../../assests/char-img.png'
 import './Info.css'
 function Info() {
+  var storedData = JSON.parse(localStorage.getItem('formData'));
+  var storedGenre = JSON.parse(localStorage.getItem('selectedGenres'));
+
+
   return (
     <div className='info'>
-      <div className='char_img'>
-        <img className='profile_img' src= {ProfileImg} alt='profile'/>
+      <img className='profile_img' src={ProfileImg} alt='profile' />
+      <div className='user_selected'>
+        <div className='user_info'>
+          <p>{storedData.name}</p>
+          <p>{storedData.email}</p>
+          <h2>{storedData.username}</h2>
+        </div>
+        <div className='user_genre'>
+          {storedGenre.slice(0,8).map((data) => (
+            <p>{data}</p>
+          )
+          )}
+        </div>
       </div>
-    </div>  
+    </div>
   )
 }
 
