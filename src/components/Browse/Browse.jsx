41d0a3388e0e209c80d58a4453f827951ac7c9bd
Abhishek Agarwal <agrawal.abhishek120201@gmail.com>
2023-08-24 23:55:15 +0530

    added all notes section

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Info from './Info';
+import Weather from './Weather';
+import Notes from './Notes';
 import './Browse.css'
 function Browse() {
   return (
-    <div className='container3'>
-    <Info/>
-    </div>
+    <>
+      <div className='container3'>
+        <div className='vertical'>
+          <Info />
+          <Weather />
+        </div>
+
+        <div className='horizontal'>
+          <Notes />
+        </div>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/Browse/Notes.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import './Notes.css'
+import { useState } from 'react';
+function Notes() {
+
+  const [Text, setText] = useState('')
+
+  const UpdateText = (e) => {
+    const newText = e.target.value;
+    setText(newText);
+    localStorage.setItem('TextArea', newText);
+  };
+
+  useEffect(() => {
+    const savedText = localStorage.getItem('TextArea');
+    if (savedText) {
+      setText(savedText);
+    }
+  }, []);
+
+  return (
+    <div className='textarea'>
+      <p className='disabledText' disabled><b>All Notes</b><br /><br />This is how I am going to learn MERN Stack in next 3 months.</p>
+      <textarea className='text' value={Text} onChange={UpdateText}></textarea>
+    </div>
+  )
+}
+
+export default Notes;
