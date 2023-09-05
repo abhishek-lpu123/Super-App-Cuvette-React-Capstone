2639a0d6338947d544c9c47c3f1e684cf9caf9e9
Abhishek Agarwal <agrawal.abhishek120201@gmail.com>
2023-09-05 21:06:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Front from './components/Home/FrontPage.jsx';
 import Category from './components/Category/Category.jsx';
 import Browse from './components/Browse/Browse.jsx';
 import Movies from './components/Movies/Movies.jsx'
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={ <Front/>} />
-        <Route path="/category" element={ <Category/>} />
-        <Route path="/browse" element={<Browse/>} />
-        <Route path='/movies' element={<Movies/>} />
+        <Route path="/" element={<Front />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path='/movies' element={<Movies />} />
       </Routes>
     </div>
   );
--- src/components/Browse/News.jsx
+++ src/components/Browse/News.jsx
@@ -1,52 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import './News.css'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import './News.css';
+import { useNavigate } from 'react-router-dom';
 
 function News() {
+  const redirectTo = useNavigate();
+  const [news, setNews] = useState('');
+  const [articleIndex, setArticleIndex] = useState(0); // Initialize with 0 instead of 1
 
-    const redirectTo = useNavigate();
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const result = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=f0080e4784ae485ba2add0c338c84229");
+        const data = await result.json();
+        setNews(data);
+        console.log(data);
+      } catch (error) {
+        console.log("Error fetching the news", error);
+      }
+    };
+    fetchNews();
+  }, []);
 
-    const [news, setNews] = useState('')
-    const [articleIndex, setArticleIndex] = useState(1);
-    useEffect(() => {
-        const fetchNews = async () => {
-            try {
-                const result = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=f0080e4784ae485ba2add0c338c84229");
-                const data = await result.json();
-                setNews(data);
-                console.log(data)
-            } catch (error) {
-                console.log("Error fetching the news", error);
-            }
-        }
-        fetchNews();
-    }, [])
-
-    const nextNews = () => {
-        const newIndex = (articleIndex + 1) % news.articles.length;
-        setArticleIndex(newIndex);
+  const nextNews = () => {
+    // Check if news and news.articles are defined
+    if (news && news.articles) {
+      const newIndex = (articleIndex + 1) % news.articles.length;
+      setArticleIndex(newIndex);
     }
+  };
 
-    const handleNextPageClick = () => {
-        redirectTo('/movies')
-    }
+  const handleNextPageClick = () => {
+    redirectTo('/movies');
+  };
 
-    return (
-        <div>
-            {news ?
-                <div className='News'>
-                    <div><img className='NewsImg' src={news.articles[articleIndex].urlToImage} alt="News Article" />
-                        <div className='NewsTitle'>
-                            <h2 className='Title'>{news.articles[articleIndex].title}</h2><br />
-                            <p className='DateTime'>{news.articles[articleIndex].publishedAt}</p></div>
-                    </div>
-                    <p className='NewsText'>{news.articles[articleIndex].description} <span className='ReadMore'><a href={news.articles[articleIndex].url} target='_blank'>Click here to read more...</a></span>
-                        <h4 className='nextNews' onClick={nextNews}>Next News →</h4></p>
-                </div> : <></>
-            }
-            <button id="nextBtn2" onClick={handleNextPageClick}><b>Next Page</b></button>
+  return (
+    <div>
+      {news && news.articles && news.articles.length > 0 && (
+        <div className='News'>
+          <div>
+            <img className='NewsImg' src={news.articles[articleIndex].urlToImage} alt="News Article" />
+            <div className='NewsTitle'>
+              <h2 className='Title'>{news.articles[articleIndex].title}</h2><br />
+              <p className='DateTime'>{news.articles[articleIndex].publishedAt}</p>
+            </div>
+          </div>
+          <p className='NewsText'>{news.articles[articleIndex].description} <span className='ReadMore'><a href={news.articles[articleIndex].url} target='_blank' rel='noopener noreferrer'>Click here to read more...</a></span>
+            <h4 className='nextNews' onClick={nextNews}>Next News →</h4>
+          </p>
         </div>
-    )
+      )}
+      <button id="nextBtn2" onClick={handleNextPageClick}><b>Next Page</b></button>
+    </div>
+  );
 }
 
-export default News
+export default News;
